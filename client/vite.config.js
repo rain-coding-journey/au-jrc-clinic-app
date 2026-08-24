af3f57bad452dd4b7981b3a6c90f6e8dc9401b64
Russell <russellandrei15@gmail.com>
2026-08-24 18:38:52 +0800

    Update CSP meta tag and disable production sourcemaps

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/au-jrc-clinic-app/',
   build: {
-    sourcemap: false, // Prevents eval-based sourcemaps in production build
-  }
-})
+    sourcemap: false, // Disables eval-based sourcemaps in production
+  },
+});
